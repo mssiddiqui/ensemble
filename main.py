d241097dce00e3c56b0c08dde6cac850eb57d2df
Mohammad Shahid Siddiqui <m.shahid.siddiqui@gmail.com>
2018-12-05 00:20:50 +1300

    Added comment to function hello_world

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 def hello_world():
+    #Adding comment just for sake of comment only.
     print ('Hellowww from a function')
+    
+    
     
 if __name__=='__main__':
     print ("Hello World!")
